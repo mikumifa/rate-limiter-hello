79b062cac49e452ae96a84b2d10322d7d95a86e4
mikumifa <1055069518@qq.com>
2023-08-10 14:18:46 +0800

    a

--- src/main/java/org/example/controller/HelloWorldController.java
+++ src/main/java/org/example/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+import io.micrometer.core.annotation.Timed;
 import org.example.service.MyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 class HelloWorldController {
 
     private final MyService myService;
-
+    @Timed(value = "api.qps")
     @GetMapping("hello")
     String sayHello() {
         return myService.hello();
